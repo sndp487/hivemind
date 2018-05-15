3c29c671aa0bda5be1e4d33e2a97b47b18193d74
roadscape <roadscape@users.noreply.github.com>
2018-05-15 18:02:46 -0500

    use port 8080, echo current age

--- scripts/hivesync.sh
+++ scripts/hivesync.sh
@@ -8,7 +8,7 @@ if [[ ! $? -eq 0 ]]; then
 
 else
     # returns 200 if head is < 15s old, signaling sync is complete. else, 500.
-    HTTP_CODE=`curl -I -s -o /dev/null -w "%{http_code}" http://127.0.0.1/head_age`
+    HTTP_CODE=`curl -I -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8080/head_age`
     if [[ ${HTTP_CODE} -eq 200 ]]; then
         echo hivemindsync: sync complete, waiting for hive to exit cleanly...
         kill -SIGINT $HIVESYNC_PID
@@ -16,6 +16,8 @@ else
         FILE_NAME=hivemind-$SCHEMA_HASH-`date '+%Y%m%d-%H%M%S'`.tar.lz4
     else
         # hive is still syncing, not complete... check back in a minute
+        HEAD_AGE=`curl -s http://127.0.0.1:8080/head_age`
+        echo hivemindsync: current head age is $HEAD_AGE seconds
         sleep 60
         exit 0
     fi
